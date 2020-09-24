3f6d46800aa633210da7a9eb453df06b586d073a
leanndemetro <leanndemetro@gmail.com>
2020-09-24 11:40:33 -0400

    created working buttons and linked APIs

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,83 @@
 // f84ef5063c2945efa425f9a031406685 API KEY for spoonacular
 
-$("#foodBtn").on("click", function(e){
+//logs the input from searchBar element when the foodBtn is clicked
+$("#foodBtn").on("click", function(e) {
     e.preventDefault();
     
+    //sets a variable that is equal to the value input into the searchBar
+    var foodInput = $("#searchBar").val();
+
+    //calls food function
+ food(foodInput);
+
+});
+
+
+//logs the input from searchBar element to the console when the foodBtn is clicked
+$("#drinkBtn").on("click", function(e) {
+    e.preventDefault();
+
+    //sets a variable that is equal to the value input into the searchBar
+    var drinkInput = $("#searchBar").val();
+
+    //calls drink function
+    drinks(drinkInput);
+
+
+});
+
+//logs a random recipe to the console when the pickBtn is picked 
+$("#pickBtn").on("click", function(e) {
+    e.preventDefault();
+
+    //calls the randomFood function
+    randomFood();
+
+//create a function the connects the searchBar input to the food function when foodBtn is clicked
+
 })
+    //creates a function that calls the API and returns an object to the console
+    function food(foodInput) {
 
-function food(){
+        $.ajax({
 
-$.ajax({
+            method: "GET",
+            url: "https://api.spoonacular.com/recipes/complexSearch?query=" + foodInput + "&apiKey=f84ef5063c2945efa425f9a031406685"
+        }).then(function(response) {
 
-    method: "GET",
-    url: "https://api.spoonacular.com/recipes/complexSearch?query=" + "spaghetti" + "&apiKey=f84ef5063c2945efa425f9a031406685"
-}).then(function (response){
+            console.log(response);
 
-    console.log(response);
+        })
+    };
 
-})};
+    //creates a function that calls the API and returns an object to the console
+    function drinks(drinkInput) {
 
-function drinks() {
+        $.ajax({
+            method: "GET",
+            url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkInput
 
-$.ajax({
-    method: "GET",
-    url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+        }).then(function (data) {
 
-}).then(function(data){
+            console.log(data);
 
-    console.log(data);
+        })
+    };
 
-})};
+    //creates a function that calls a random food API and logs the result in the console 
+    function randomFood() {
+
+    $.ajax({
+        method: "GET",
+        url: "https://api.spoonacular.com/recipes/random?apiKey=f84ef5063c2945efa425f9a031406685"
     
+    }).then(function(result) {
 
-drinks();
-food();
+        console.log(result);
+
+    })
+};
+
+   
+
+
