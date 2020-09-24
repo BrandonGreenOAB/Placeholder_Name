4dd22198f3f16e3a37c5b513d8b1af6984c8fba9
Bridget Shannon <bridgetrshannon@gmail.com>
2020-09-24 14:32:05 -0400

    Closes #16

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,29 @@
 // f84ef5063c2945efa425f9a031406685 API KEY for spoonacular
 
+var foodStorage = [];
 //logs the input from searchBar element when the foodBtn is clicked
-$("#foodBtn").on("click", function(e) {
+
+$("#foodBtn").on("click", function (e) {
     e.preventDefault();
-    
+
+    // make main box go blank with each click
+
+    $("#mainBox").html("")
+
     //sets a variable that is equal to the value input into the searchBar
     var foodInput = $("#searchBar").val();
 
     //calls food function
- food(foodInput);
+    food(foodInput);
 
+    store(foodInput);
 
+    appendSearch();
 
 });
 
-
 //logs the input from searchBar element to the console when the foodBtn is clicked
-$("#drinkBtn").on("click", function(e) {
+$("#drinkBtn").on("click", function (e) {
     e.preventDefault();
 
     //sets a variable that is equal to the value input into the searchBar
@@ -29,70 +36,93 @@ $("#drinkBtn").on("click", function(e) {
 });
 
 //logs a random recipe to the console when the pickBtn is picked 
-$("#pickBtn").on("click", function(e) {
+$("#pickBtn").on("click", function (e) {
     e.preventDefault();
 
     //calls the randomFood function
     randomFood();
 
-//create a function the connects the searchBar input to the food function when foodBtn is clicked
+    //create a function the connects the searchBar input to the food function when foodBtn is clicked
 
 })
-    //creates a function that calls the API and returns an object to the console
-    function food(foodInput) {
+//creates a function that calls the API and returns an object to the console
+function food(foodInput) {
 
-        $.ajax({
+    $.ajax({
 
-            method: "GET",
-            url: "https://www.themealdb.com/api/json/v1/1/search.php?s=" + foodInput 
+        method: "GET",
+        url: "https://www.themealdb.com/api/json/v1/1/search.php?s=" + foodInput
 
-        }).then(function(response) {
+    }).then(function (response) {
 
-            console.log(response);
+        console.log(response);
 
-            var foodResults = $("<p>");
+        var foodResults = $("<p>");
 
-            foodResults.text(response.meals[0].strIngredient1);
+        foodResults.text(response.meals[0].strIngredient1);
 
-            $("#mainBox").append(foodResults);
+        $("#mainBox").append(foodResults);
 
-            for (let index = 1; index <= 20; index++) {
-                console.log(response.meals[0]["strIngredient" + index]);
+        for (let index = 1; index <= 20; index++) {
+            console.log(response.meals[0]["strIngredient" + index]);
 
-                }
+        }
 
-        })
-    };
+    })
+};
 
-    //creates a function that calls the API and returns an object to the console
-    function drinks(drinkInput) {
+//creates a function that calls the API and returns an object to the console
+function drinks(drinkInput) {
 
-        $.ajax({
-            method: "GET",
-            url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkInput
+    $.ajax({
+        method: "GET",
+        url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkInput
 
-        }).then(function (data) {
+    }).then(function (data) {
 
-            console.log(data);
+        console.log(data);
 
-        })
-    };
+    })
+};
 
-    //creates a function that calls a random food API and logs the result in the console 
-    function randomFood() {
+//creates a function that calls a random food API and logs the result in the console 
+function randomFood() {
 
     $.ajax({
         method: "GET",
         url: "https://api.spoonacular.com/recipes/random?apiKey=f84ef5063c2945efa425f9a031406685"
-    
-    }).then(function(result) {
+
+    }).then(function (result) {
 
         console.log(result);
 
     })
 };
 
+function store(foodInput) {
+    // //stores searcHistory array in local storage under the key name recentSearch
+    // localStorage.setItem("foodStorage", foodInput);
 
-   
+    // // 
+    // localStorage.setItem("foodStorage", JSON.stringify(foodStorage));
+
+    var searchlocal = JSON.parse(localStorage.getItem("foodStorage"))
+    console.log(searchlocal);
+    if (searchlocal === null) {
+        foodStorage = [foodInput];
+    } else {
+        foodStorage.push(foodInput)
+    }
+    console.log(foodStorage);
+    localStorage.setItem("foodStorage", JSON.stringify(foodStorage));
+}
+
+function appendSearch() {
+
+    var appendLeft = $("<button>");
+    appendLeft.text(foodStorage[foodStorage.length - 1]);
+
+    $("#leftSide").append(appendLeft);
 
 
+} 
